035a2187119b5f6d5725cae13d27fbf31d35252e
Cor van Doorn <cor.vandoorn@vixion.nl>
2018-06-19 12:00:29 +0200

    Payment model

---
+++ MagicCarpetWebApp/Models/Payment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicCarpetWebApp.Models
+{
+    public class Payment
+    {
+        public Guid ReservationId { get; set; }
+        public double Price { get; set; }
+    }
+}
